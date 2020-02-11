d847cf899d0bb58cfe8b9ddfc56148d304312af6
Carlos Vaz <carlos-vaz@procergs.rs.gov.br>
2020-02-11 10:39:20 -0300

    refatorando

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -7,24 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: (Text('1000')),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.0'),
-                subtitle: (Text('1000')),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(title: Text('Transferências'),),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
@@ -32,4 +15,46 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+
+}
+
+class Transferencia {
+
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+
 }
