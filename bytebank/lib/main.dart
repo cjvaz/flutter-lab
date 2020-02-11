3410c31bb04e3abec24e70f2efb6985552474122
Carlos Vaz <carlos-vaz@procergs.rs.gov.br>
2020-02-11 12:16:11 -0300

    apresentando mensagens com SnackBar

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -61,6 +61,11 @@ class FormularioTransferencia extends StatelessWidget {
                 if(numeroConta != null && valor != null) {
                   final transferenciaCriada = Transferencia(valor, numeroConta);
                   debugPrint('$transferenciaCriada');
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('$transferenciaCriada'),
+                    ),
+                  );
                 }
               },
               child: Text('Confirmar'),
