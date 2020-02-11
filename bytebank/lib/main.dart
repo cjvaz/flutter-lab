f8fdac3a5894199fd2d14d6c3c9b71bdf9f8bc38
Carlos Vaz <carlos-vaz@procergs.rs.gov.br>
2020-02-11 10:16:05 -0300

    add listTile

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -7,6 +7,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: (Text('1000')),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('200.0'),
+                subtitle: (Text('1000')),
+              ),
+            ),
+          ],
+        ),
         appBar: AppBar(title: Text('Transferências'),),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
