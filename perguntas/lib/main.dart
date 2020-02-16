672b8cde52187b981d24ea659815a4a97a810f7f
Carlos Vaz <carlosjvaz@gmail.com>
2020-02-16 08:02:33 -0300

    [perguntas] - Init project

---
+++ perguntas/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Óla Flutter!!'),
+    );
+  }
+}
