517944bd7d1fd7712c3f30198607efa3a0b87579
Carlos Vaz <carlosjvaz@gmail.com>
2020-02-17 20:33:31 -0300

    testes design app perguntas

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -6,8 +6,34 @@ class PerguntaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
-      home: Text('Óla Flutter!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
